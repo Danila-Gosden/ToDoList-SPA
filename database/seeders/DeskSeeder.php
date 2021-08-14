b07bc393de860fe3d3562fb18e8855dac14bdd15
Danila-Gosden <danilagosden@gmail.com>
2021-08-14 13:29:42 +0300

    Showed Data in JSON

--- database/seeders/DeskSeeder.php
+++ database/seeders/DeskSeeder.php
@@ -2,17 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Models\Desk;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class DeskSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Desk::factory()->count(5)->create();
     }
 }
